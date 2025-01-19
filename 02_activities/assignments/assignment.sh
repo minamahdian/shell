d919a4c77be0a2ffb141483af19710311ca6196d
minamahdian <mah.mina154@gmail.com>
2025-01-19 11:41:04 -0500

    I complete the assignment

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -39,5 +39,6 @@ rm ./data/processed/user_logs/*ipaddr*
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
 find ./data/processed -type f > ./data/inventory.txt
 ###########################################
-echo "Project1 setup is complete!"
+echo "Project1 setup is complete1!"
 git pull https://github.com/UofT-DSI/shell coworker-changes --no-rebase
+git status
